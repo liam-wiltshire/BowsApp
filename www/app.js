88bcf0629ba70a49ae34908957a315c435ac55dc
Liam Wiltshire <liam@w.iltshi.re>
2016-08-12 14:25:41 +0100

    Fixes

--- www/app.js
+++ www/app.js
@@ -127,14 +127,15 @@
 			actions.processAjax("act=getScans&username="+global.username+"&registration_id="+global.registration_id+"",function(data){
 				x = 0;
 				html = "";
+				alert(data.length);
 				while (x < data.length){
-					scan = data[x];
-					html += '<div class="latest-today">\
-						<h4>PT: '+scan.tick+'</h4>\
-					<h3>'+scantypes[scan.scantype].name+' on '+scan.x+':'+scan.y+':'+scan.z+'</h3>\
-					<p><a class="btn btn-primary" target="_blank" href="http://game.planetarion.com/waves.pl?id='+scantypes[scan.scantype].id+'&x='+scan.x+'&y='+scan.y+'&z='+scan.x+'">Scan</a> Requested by: <span class="todt-joe"> '+scan.name+' </span></p>\
-					</div>';
-					x++;
+					try {
+						scan = data[x];
+						html += '<div class="latest-today"><h4>PT: ' + scan.tick + '</h4><h3>' + scantypes[scan.scantype].name + ' on ' + scan.x + ':' + scan.y + ':' + scan.z + '</h3><p><a class="btn btn-primary" target="_blank" href="http://game.planetarion.com/waves.pl?id=' + scantypes[scan.scantype].id + '&x=' + scan.x + '&y=' + scan.y + '&z=' + scan.x + '">Scan</a> Requested by: <span class="todt-joe"> ' + scan.name + ' </span></p></div>';
+						x++;
+					}catch (e){
+						alert(e);
+					}
 				}
 
 				alert(html);
