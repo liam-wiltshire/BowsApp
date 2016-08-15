e701366c59e1dce3d6787604a8e9ed8c9d980f98
Liam Wiltshire <liam@w.iltshi.re>
2016-08-15 23:21:06 +0100

    Style Fixes

--- www/app.js
+++ www/app.js
@@ -114,7 +114,7 @@
 			});
 		},
 
-		sendMessage : function(){
+		sendMessage : function(text){
 			url = "act=sendMsg&username="+global.username+"&registration_id="+global.registration_id+"&msg="+encodeURIComponent(text)+"";
 			alert(url);
 			actions.processAjax(url,function(data){
